ce81b9c7b9391d159aa0e49b2ee7ac83fe54162b
Tim Vlasblom <tim@competa.com>
2019-03-13 14:47:01 +0100

    added basic initialization in main

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class main
+{
+    public int boardSize = 9;
+
+    public static void main(String [] args)
+    {
+        Gameboard gameboard = new Gameboard;
+        Avatar avatar = new Avatar;
+    }
+
+}
