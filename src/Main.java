6b7785c284dcf86f72e45cd2a3a0e4c2e9045c3b
IYIatthys <31695309+IYIatthys@users.noreply.github.com>
2019-03-14 15:21:22 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
-public class main
+import Objects.Avatar;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main
 {
     public int boardSize = 9;
 
-    public static void main(String [] args)
+    public static void main(String[] args)
     {
-        Gameboard gameboard = new Gameboard;
-        Avatar avatar = new Avatar;
+        //Gameboard gameboard = new Gameboard;
+        Avatar avatar = new Avatar();
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(1920, 1080);
+
+        JPanel bord = new JPanel();
+        bord.setLayout(new GridLayout(5,5));
+
+
+        for(int i = 0; i < 25; i++){
+            bord.add(new JButton());
+        }
+
+
+        frame.add(bord);
+        frame.setVisible(true);
     }
 
 }
