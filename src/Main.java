d182c58aab2ac07bc7d54d92e420b8db2602ac54
timVlasblom <tim1998@live.nl>
2019-03-20 14:22:45 +0100

    made functions in the main

--- src/Main.java
+++ src/Main.java
@@ -6,44 +6,56 @@ import java.awt.*;
 
 public class Main
 {
+    //Size of the frame
+    int frameWidth = 1000;
+    int frameHeight = 1000;
+
+    //Size of the gameboard
+    int row = 9;
+    int col = 9;
+
+    Avatar avatar = new Avatar();
+
+    JPanel gameboard = new JPanel();
+    JFrame frame = new JFrame();
+
     public static void main(String[] args)
     {
-        //Size of the frame
-        int frameWidth = 1000;
-        int frameHeight = 1000;
+        Main main = new Main();
+        main.setup();
+    }
 
-        //Size of the gameboard
-        int row = 9;
-        int col = 9;
-
-        Avatar avatar = new Avatar();
-
+    public void setup(){
         //Setup the frame
-        JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(frameWidth, frameHeight);
         frame.setResizable(false);
 
         //Setup the play field
-        JPanel gameboard = new JPanel();
         gameboard.setLayout(new GridLayout(row,col));
 
         //Fill the frame with squares
         Square[][] squares = new Square[row][col];
 
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                squares[i][j] = new Square();
-
-                gameboard.add(squares[i][j]);
-            }
-        }
+        drawBoard(squares);
 
         //An avatar is a square, but with different methods and color
-        squares[0][0].setBackground(Color.red);
 
         frame.add(gameboard);
         frame.setVisible(true);
+    }
+
+    public void drawBoard(Square squares[][]){
+        squares[avatar.getRow()][avatar.getCol()] = avatar;
+        gameboard.removeAll();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if (squares[i][j] != avatar){
+                    squares[i][j] = new Square();
+                }
+                gameboard.add(squares[i][j]);
+            }
+        }
     }
 
 }
