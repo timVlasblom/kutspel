9534682ce476d96c5dcdf42ae2d4a2aa34868fa9
Jimmyvb98 <jimmyvanbarneveld@live.nl>
2019-03-20 15:14:21 +0100

    added keyevents

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import Objects.Square;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
-public class Main
-{
+public class Main {
     //Size of the frame
     int frameWidth = 1000;
     int frameHeight = 1000;
@@ -14,30 +14,31 @@ public class Main
     int row = 9;
     int col = 9;
 
+    Square[][] squares = new Square[row][col];
+
     Avatar avatar = new Avatar();
 
     JPanel gameboard = new JPanel();
     JFrame frame = new JFrame();
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Main main = new Main();
         main.setup();
     }
 
-    public void setup(){
+    public void setup() {
         //Setup the frame
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(frameWidth, frameHeight);
         frame.setResizable(false);
 
         //Setup the play field
-        gameboard.setLayout(new GridLayout(row,col));
+        gameboard.setLayout(new GridLayout(row, col));
 
         //Fill the frame with squares
-        Square[][] squares = new Square[row][col];
 
-        drawBoard(squares);
+
+        drawBoard();
 
         //An avatar is a square, but with different methods and color
 
@@ -45,17 +46,16 @@ public class Main
         frame.setVisible(true);
     }
 
-    public void drawBoard(Square squares[][]){
+    public void drawBoard() {
         squares[avatar.getRow()][avatar.getCol()] = avatar;
         gameboard.removeAll();
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if (squares[i][j] != avatar){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (squares[i][j] != avatar) {
                     squares[i][j] = new Square();
                 }
                 gameboard.add(squares[i][j]);
             }
         }
     }
-
 }
--- src/Objects/Avatar.java
+++ src/Objects/Avatar.java
@@ -1,26 +1,41 @@
 package Objects;
 
+import com.sun.javaws.Main;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
-public class Avatar extends Square{
+public class Avatar extends Square {
     private int x;
     private int y;
 
-        public Avatar(){
-            super();
-            setBackground(Color.red);
+    public Avatar() {
+        super();
+        setBackground(Color.red);
 
-            x=0;
-            y=0;
-        }
+        x = 0;
+        y = 0;
+    }
 
-        public int getRow(){
-            return this.x;
-        }
+    public int getRow() {
+        return this.x;
+    }
 
-        public int getCol(){
-            return this.y;
+    public int getCol() {
+        return this.y;
+    }
+
+    public void keyPressed(KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.VK_UP || event.getKeyCode() == KeyEvent.VK_W) {
         }
+        if (event.getKeyCode() == KeyEvent.VK_DOWN || event.getKeyCode() == KeyEvent.VK_S) {
+        }
+        if (event.getKeyCode() == KeyEvent.VK_LEFT || event.getKeyCode() == KeyEvent.VK_A) {
+        }
+        if (event.getKeyCode() == KeyEvent.VK_RIGHT || event.getKeyCode() == KeyEvent.VK_D) {
+            y += 1;
+            Main.drawBoard();
+        }
+    }
 }
-
