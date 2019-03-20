9e70736323ce48e1b15f3b22fba0c203637d0a68
IYIatthys <31695309+IYIatthys@users.noreply.github.com>
2019-03-20 12:30:28 +0100

    Opzet bord

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,48 @@
 import Objects.Avatar;
+import Objects.Square;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Main
 {
-    public int boardSize = 9;
-
     public static void main(String[] args)
     {
-        //Gameboard gameboard = new Gameboard;
+        //Size of the frame
+        int frameWidth = 1000;
+        int frameHeight = 1000;
+
+        //Size of the gameboard
+        int row = 9;
+        int col = 9;
+
         Avatar avatar = new Avatar();
 
+        //Setup the frame
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(1920, 1080);
+        frame.setSize(frameWidth, frameHeight);
+        frame.setResizable(false);
 
-        JPanel bord = new JPanel();
-        bord.setLayout(new GridLayout(5,5));
+        //Setup the play field
+        JPanel gameboard = new JPanel();
+        gameboard.setLayout(new GridLayout(row,col));
 
+        //Fill the frame with squares
+        Square[][] squares = new Square[row][col];
 
-        for(int i = 0; i < 25; i++){
-            bord.add(new JButton());
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                squares[i][j] = new Square();
+
+                gameboard.add(squares[i][j]);
+            }
         }
 
+        //An avatar is a square, but with different methods and color
+        squares[0][0].setBackground(Color.red);
 
-        frame.add(bord);
+        frame.add(gameboard);
         frame.setVisible(true);
     }
 
