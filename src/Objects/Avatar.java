8460dec433770ee508f7eda476f39d382673a9e3
Jimmy van Barneveld <48517376+Jimmyvb98@users.noreply.github.com>
2019-03-26 14:27:42 +0100

    fillRect * 100

--- src/Objects/Avatar.java
+++ src/Objects/Avatar.java
@@ -8,8 +8,6 @@ import java.awt.event.KeyListener;
 public class Avatar extends Square implements KeyListener {
     private int x;
     private int y;
-    private int x1;
-    private int y1;
 
     //Sets up the avatar class
     public Avatar(Gameboard gameboard) {
@@ -18,8 +16,6 @@ public class Avatar extends Square implements KeyListener {
 
         x = 0;
         y = 0;
-        x1 = 0;
-        y1 = 0;
         setFocusable(true);
         addKeyListener(this);
     }
@@ -37,7 +33,7 @@ public class Avatar extends Square implements KeyListener {
     //Makes the player (avatar) red and sets the start location
     public void paintComponent(Graphics g) {
         g.setColor(Color.RED);
-        g.fillRect(x1, y1, 50, 50);
+        g.fillRect(x * 100, y * 100, 50, 50);
         setOpaque(false);
         repaint();
     }
@@ -49,34 +45,26 @@ public class Avatar extends Square implements KeyListener {
     //Detects if any of the desired keys is pressed and moves the avatar
     public void keyPressed(KeyEvent event) {
         if (event.getKeyCode() == KeyEvent.VK_UP || event.getKeyCode() == KeyEvent.VK_W) {
-            y1 -= 100;
             y -= 1;
             if (y <= -1) {
-                y1 = 0;
                 y = 0;
             }
         }
         if (event.getKeyCode() == KeyEvent.VK_DOWN || event.getKeyCode() == KeyEvent.VK_S) {
-            y1 += 100;
             y += 1;
             if (y > 9) {
-                y1 = 900;
                 y = 9;
             }
         }
         if (event.getKeyCode() == KeyEvent.VK_LEFT || event.getKeyCode() == KeyEvent.VK_A) {
-            x1 -= 100;
             x -= 1;
             if (x <= -1) {
-                x1 = 0;
                 x = 0;
             }
         }
         if (event.getKeyCode() == KeyEvent.VK_RIGHT || event.getKeyCode() == KeyEvent.VK_D) {
-            x1 += 100;
             x += 1;
             if (x > 9) {
-                x1 = 900;
                 x = 9;
             }
         }
