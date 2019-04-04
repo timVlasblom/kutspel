6ae5df3576a28f1c73afc7feb0cff3cd58f4cd51
Jimmyvb98 <jimmyvanbarneveld@live.nl>
2019-04-04 15:05:49 +0200

    update

--- src/test/kutspel/Objects/GameboardTest.java
+++ src/test/kutspel/Objects/GameboardTest.java
@@ -26,4 +26,23 @@ public class GameboardTest {
         gameboard.startLevel();
         assertTrue(gameboard.board[3][3] instanceof Wall);
     }
+
+    @Test
+    public void exitTest(){
+        gameboard.startLevel();
+        assertTrue(gameboard.board[9][9] instanceof Exit);
+    }
+    @Test
+
+    public void keyTest(){
+        gameboard.startLevel();
+        assertTrue(gameboard.board[2][2] instanceof Key);
+    }
+
+    @Test
+    public void barricadeTest(){
+        gameboard.startLevel();
+        assertTrue(gameboard.board[6][6] instanceof Barricade);
+    }
+
 }
